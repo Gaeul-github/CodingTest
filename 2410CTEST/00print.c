f820cc02a2ed82d68bef2edab76dedfb81cce066
Gaeul-github <99901580+Gaeul-github@users.noreply.github.com>
2022-03-21 12:06:49 +0900

    Hello World %c로 찍기

--- 2410CTEST/00print.c
+++ 2410CTEST/00print.c
@@ -12,5 +12,23 @@ int main(void) {
 
 	printf("정수: %d, 문자: %c, 더블: %lf, 플롯: %f\n", jungsu, munja, sosu, sosu1);
 	printf("문자열: %s", mjyeol);
+
+/*	for (int i = 0; i <= 11; i++) {
+		printf("%c", mjyeol[i]);
+	}
+	*/
+
+	printf("%c", mjyeol[1]);
+	printf("%c", mjyeol[2]);
+	printf("%c", mjyeol[3]);
+	printf("%c", mjyeol[4]);
+	printf("%c", mjyeol[5]);
+	printf("%c", mjyeol[6]);
+	printf("%c", mjyeol[7]);
+	printf("%c", mjyeol[8]);
+	printf("%c", mjyeol[9]);
+	printf("%c", mjyeol[10]);
+	printf("%c", mjyeol[11]);
+	
 	return 0;
 }

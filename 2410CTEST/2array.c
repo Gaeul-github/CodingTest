f12ec20390c7712793fd489430a7d11ba77ae510
Gaeul-github <99901580+Gaeul-github@users.noreply.github.com>
2022-04-04 12:00:47 +0900

    ㄷ더더 유연한 1차원 배열

--- 2410CTEST/2array.c
+++ 2410CTEST/2array.c
@@ -3,7 +3,7 @@
 int main() {
 	int arr[12] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
 
-	for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
 		printf("%d\n",arr[i]);
 	}
 }

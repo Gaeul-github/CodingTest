b06166bece65879bc8d700ea502c4f8a8dfba28b
Gaeul-github <99901580+Gaeul-github@users.noreply.github.com>
2022-04-11 10:50:56 +0900

    2차원 배열 살짝 유식하게

--- 2410CTEST/2array.c
+++ 2410CTEST/2array.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main() {
-	int arr[12] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	int arr[3][4] = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
 
-	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
-		printf("%d\n",arr[i]);
+
+	for (int i = 0; i < 3; i++) {
+		printf("%d\n", arr[i][0]);
+		printf("%d\n", arr[i][1]);
+		printf("%d\n", arr[i][2]);
+		printf("%d\n", arr[i][3]);
 	}
+	
 }
